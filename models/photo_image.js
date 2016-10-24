a6fc37c90ad7700fdcd83b64b75575e02f151f48
cn27529 <cn27529@hotmail.com>
2016-10-24 17:40:39 +0800

    20161024

--- models/photo_image.js
+++ models/photo_image.js
@@ -6,14 +6,20 @@ module.exports = function(sequelize, DataTypes) {
     var PhotoImage = sequelize.define("Photo_image", {
         title: DataTypes.STRING,
         image: DataTypes.TEXT,
-        //credate: DataTypes.STRING,
-        //photoid: DataTypes.INTEGER,
-        //profileid: DataTypes.INTEGER
+        PhotoId: DataTypes.INTEGER,
+        ProfileId: DataTypes.INTEGER
     }, {
-        // 如果为 true 则表的名称和 model 相同，
-        // 为 false MySQL创建的表名称加上复数s
-        // 如果指定的表名称本就是复数形式则不变
-        //freezeTableName: false
+      freezeTableName: false,
+      classMethods: {
+        associate: function(models) {
+          PhotoImage.belongsTo(models.Photo, {
+            onDelete: "CASCADE",
+            foreignKey: {
+              allowNull: false
+            }
+          });
+        }
+      }
     });
 
     return PhotoImage;
