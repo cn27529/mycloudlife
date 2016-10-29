723386acf8a6433d140aff17a1124436fbed71b3
cn27529 <cn27529@hotmail.com>
2016-10-29 19:24:13 +0800

    force=true and mail.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var models = require('./models');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '5000');
+var port = normalizePort(process.env.PORT || '80');
 app.set('port', port);
 /**
  * Create HTTP server.
