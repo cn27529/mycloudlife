6a90ac0319e4f4f0a1a17391c8a5a80863eeba19
cn27529 <cn27529@hotmail.com>
2016-10-29 23:24:48 +0800

    8080

--- models/maillog.js
+++ models/maillog.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
     //https://cn27529.gitbooks.io/mycloudlife-book/content/note_table.html
     var Maillog = sequelize.define("Maillog", {
         title: DataTypes.STRING,
-        body: DataTypes.TEXT,
+        body: DataTypes.STRING,
         mailForm: DataTypes.STRING,
         mailTo: DataTypes.STRING,
         msg: DataTypes.STRING,
