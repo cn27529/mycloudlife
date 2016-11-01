7129a66de0430cb91e6ae1ef495c87174c0ee3c3
cn27529 <cn27529@hotmail.com>
2016-11-01 14:37:49 +0800

    161101d

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var models = require('./models');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '8080');
-//var port = normalizePort(process.env.PORT || '5000');
+//var port = normalizePort(process.env.PORT || '8080');
+var port = normalizePort(process.env.PORT || '5000');
+
 app.set('port', port);
 /**
  * Create HTTP server.
