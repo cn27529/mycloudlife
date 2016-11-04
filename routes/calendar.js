56f86fc571b92620d710b917704722f13e9842f2
Larry Huang <larry.huang0220@gmail.com>
2016-11-04 17:19:47 +0800

    update

--- routes/calendar.js
+++ routes/calendar.js
@@ -10,9 +10,9 @@ var router = express.Router();
 router.post('/create', function(req, res) {
   //token檢查, 先不檢查
   //var token = req.body.token;
-  var calendar;
+  var event;
   try {
-    calendar = {
+    event = {
       title: req.body.event.title,
       people: req.body.event.people.join(','),
       start: req.body.event.start,
@@ -22,17 +22,17 @@ router.post('/create', function(req, res) {
       calendar: req.body.event.calendar,
       notes: req.body.event.notes,
       mode: req.params.mode,
-      ProfileId: req.body.id
+      ProfileId: parseInt(req.body.id)
     }
 
     switch (req.body.mode) {
       case 'multiple':
-        calendar.multiple = req.body.multiple.join(',');
+        event.multiple = req.body.multiple.join(',');
         break;
       case 'repeat':
-        calendar.repeat_type = req.body.event.repeat_type;
-        calendar.repeat_detail = req.body.event.repeat_detail;
-        calendar.repeat_until = req.body.event.repeat_until;
+        event.repeat_type = req.body.event.repeat_type;
+        event.repeat_detail = req.body.event.repeat_detail;
+        event.repeat_until = req.body.event.repeat_until;
       default:
     }
   } catch (err) {
@@ -43,21 +43,80 @@ router.post('/create', function(req, res) {
   }
 
 
-  // create calendar
-  models.Calendar.create(calendar).then(function(data) {
-    // console.log(data.get({ plain: true }));
+  // create calendar & event
+  models.Calendar
+    .findOrCreate({
+      where: {
+        start: event.start,
+        ProfileId: event.ProfileId
+      },
+      defaults: {
+        start: event.start,
+        ProfileId: event.ProfileId
+      }
+    })
+    .spread(function(data) {
+      // console.log(data.get({ plain: true }));
 
-    // mode: single, multiple, repeat
-    json = {
-      "id": data.id, //這是使用者的資料代碼, 可存在用戶端
-      "msg": "ok,資料己建立",
-      "err": ""
-    };
+      event.CalendarId = parseInt(data.id);
+      models.Calendar_event
+        .create(event);
 
-    res.json(json);
-  });
+      console.log(typeof event.ProfileId, typeof event.CalendarId);
+      // mode: single, multiple, repeat
+      json = {
+        "id": event.ProfileId, //這是使用者的資料代碼, 可存在用戶端
+        "msg": "ok,資料己建立",
+        "err": ""
+      };
+
+      res.json(json);
+    });
 });
 
 
+// get by id
+router.get('/limit/:id/:start/:top', function(req, res) {
+  var id = parseInt(req.params.id);
+  var start = req.params.start;
+  var top = parseInt(req.params.top);
+  //var token = req.params.token; //先不檢查
+  var json = {
+    id: 0,
+    msg: "沒有資料",
+    err: "",
+    photos: []
+  }
+
+  models.Profile.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(data) {
+    if (data === null) {
+      return res.json(json);
+    }
+
+    var profileId = data.id;
+    json.id = data.id;
+    json.msg = "ok";
+
+    // 撈calendar的資料
+    models.Calendar.findAll({
+      where: {
+        start: start,
+        ProfileId: profileId
+      },
+      limit: top,
+      include: [{
+        model: models.Calendar_event,
+      }],
+    }).then(function(events) {
+
+      res.json(events);
+    });
+
+  });
+});
 
 module.exports = router;
