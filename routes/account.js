01450fae32e536f84527d4cd7ea2b1d5cb427aa3
cn27529 <cn27529@hotmail.com>
2016-11-06 16:54:38 +0800

    1106a

--- routes/account.js
+++ routes/account.js
@@ -7,6 +7,60 @@ var sendMail = require('../mail/sendMail');
 //文件
 //https://cn27529.gitbooks.io/mycloudlife-api/content/account.html
 
+
+//忘記密碼1106
+router.get('/repwdtest/:email', function(req, res) {
+
+    var email = req.params.email;
+    //var token = req.params.token; //先不檢查
+
+    var json = {
+        id: 0,
+        email: "",
+        msg: "沒有資料",
+        pwd: "",
+        err: ""
+    }
+
+    //props
+    var mail_data = {
+        //mailFrom: 'mycloudedlife1@gmail.com',
+        mailFrom: 'service@mycloudedlife.com',
+        mailTo: email,
+        title: '忘記密碼',
+        body: 'Dear XXX,<br>Thank you for contacting us<br>This is your password: <br>For your account security, please change your passowrd later.',
+        pwd: '1234qwer'
+    }
+
+    sendMail(mail_data.mailFrom, mail_data.mailTo, mail_data.title, mail_data.body, sendMailCallback);
+
+    //callback function
+    function sendMailCallback(mailMsg) {
+
+        console.log(mailMsg);
+        var now = new Date();
+        var month = (now.getMonth() + 1);
+        var yy = (now.getFullYear() <= 9) ? '0' + now.getFullYear().toString() : now.getFullYear().toString();
+        var mm = (month <= 9) ? '0' + month.toString() : month.toString();
+        var dd = (now.getDate() <= 9) ? '0' + now.getDate().toString() : now.getDate().toString();
+
+        if (typeof mailMsg === 'string') {
+            // this is a string
+            json.msg = mailMsg;
+            json.err = "mail";
+
+        } else {
+            json.msg = "ok,郵件己發送";
+        }
+
+        res.json(json);
+
+    }
+
+
+});
+
+
 //忘記密碼1106
 router.get('/repwd/:email', function(req, res) {
 
