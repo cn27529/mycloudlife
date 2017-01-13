dea72b465939b0a6ed7b8589b5131068fd214470
cn27529 <cn27529@hotmail.com>
2017-01-13 16:14:36 +0800

    0113

--- routes/mail.js
+++ routes/mail.js
@@ -8,6 +8,113 @@ var sendMail = require('../mail/sendMail');
 //文件
 //https://cn27529.gitbooks.io/mycloudlife-api/content/mail.html
 
+//註冊後發送註冊確認信
+router.post('/registered', function (req, res) {
+
+    var email = req.body.email;
+    var pwd = req.body.pwd;
+    var name = req.body.name;
+    var sex = req.body.sex;
+
+    var json = {
+        "email": email,
+        "pwd": pwd,
+        "name": "",
+        "sex": "",
+        "msg": "post沒有電子郵件",
+        "err": "register",
+        "mailFrom": "mailFrom",
+        "mailTo": "mailTo",
+        "title": "title",
+        "body": "body"
+    }
+
+    if (email === undefined) {
+        json.msg = "email is undefined";
+        res.json(json);
+    }
+    if (email == null) {
+        json.msg = "email is null";
+        res.json(json);
+    }
+
+    if (name === undefined) {
+        json.msg = "name is undefined";
+        res.json(json);
+    }
+    if (name == null) {
+        json.msg = "name is null";
+        res.json(json);
+    }
+
+    //props
+    var mail_data = {
+        mailFrom: 'service@mycloudedlife.com',
+        mailTo: email,
+        title: 'mycloudedlife註冊後發送註冊確認信',
+        body: 'body',
+        pwd: pwd,
+        name: name
+    }
+
+    var br = '<br>';
+    var body_lines = new Array();
+    body_lines.push('；' + 'Dear ' + name + ',');
+    body_lines.push('；；' + 'Thank you for contacting us');
+    body_lines.push('；；' + 'This is your password: ' + pwd);
+
+    var body_string = body_lines.join('。');
+    body_string = body_string.replace(/。/g, '');
+    body_string = body_string.replace(/；/g, br);
+    var body_result = body_string;
+    mail_data.body = body_result;
+    console.log(mail_data.body);
+
+    //寄送mail
+    var now = new Date();
+    var month = (now.getMonth() + 1);
+    var yy = (now.getFullYear() <= 9) ? '0' + now.getFullYear().toString() : now.getFullYear().toString();
+    var mm = (month <= 9) ? '0' + month.toString() : month.toString();
+    var dd = (now.getDate() <= 9) ? '0' + now.getDate().toString() : now.getDate().toString();
+
+    models.Maillog.create({
+        title: mail_data.title,
+        body: mail_data.body,
+        mailFrom: mail_data.mailFrom,
+        mailTo: mail_data.mailTo,
+        msg: "registered",
+        yymmdd: yy + mm + dd,
+        yymm: yy + mm
+    }).then(function (data) {
+        console.log(data);
+        //console.log('models.Maillog.create');
+    }).catch(function (err) {
+        console.log(err);
+        json.err = "sql";
+        json.msg = err;
+        res.json(json);
+    });
+
+    sendMail(mail_data.mailFrom, mail_data.mailTo, mail_data.title, mail_data.body, sendMailCallback);
+
+    json.err = "";
+    json.msg = "ok,郵件己發送";
+    res.json(json);
+
+    //callback function
+    function sendMailCallback(mailMsg, subject, html, from, to) {
+        var now = new Date();
+        var month = (now.getMonth() + 1);
+        var yy = (now.getFullYear() <= 9) ? '0' + now.getFullYear().toString() : now.getFullYear().toString();
+        var mm = (month <= 9) ? '0' + month.toString() : month.toString();
+        var dd = (now.getDate() <= 9) ? '0' + now.getDate().toString() : now.getDate().toString();
+        console.log(mailMsg);
+        console.log("sendMailCallback: " + now.toLocaleString());
+    }
+
+});
+
+
 //忘記密碼寄送mail
 router.post('/repwd', function(req, res) {
 
@@ -81,7 +188,7 @@ router.post('/repwd', function(req, res) {
         body: mail_data.body,
         mailFrom: mail_data.mailFrom,
         mailTo: mail_data.mailTo,
-        msg: "",
+        msg: "repwd",
         yymmdd: yy + mm + dd,
         yymm: yy + mm
     }).then(function(data) {
@@ -200,7 +307,7 @@ router.post('/reqemail', function(req, res) {
         body: mail_data.body,
         mailFrom: mail_data.mailFrom,
         mailTo: mail_data.mailTo,
-        msg: "",
+        msg: "reqemail",
         yymmdd: yy + mm + dd,
         yymm: yy + mm
     }).then(function(data) {
