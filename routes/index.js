f4bfad47d572e6ef11ae17b3f2f1e58564ed05d9
cn27529 <cn27529@hotmail.com>
2017-06-26 10:13:36 +0800

    0626

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var router = express.Router();
 var cool = require('cool-ascii-faces');
 
-router.get('/', function(req, res) {
-    let hello = '<h1>'+cool()+'</h1>';
+router.get('/', function (req, res) {
+    let hello = '<h1>' + cool() + '</h1>';
     res.send(hello);
     console.log(cool());
 });
