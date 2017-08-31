052015ab0fb17b39f9c4457d4b85112f475a31fa
cn27529 <cn27529@hotmail.com>
2017-08-31 16:54:44 +0800

    0831

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,28 @@ var router = express.Router();
 var cool = require('cool-ascii-faces');
 
 router.get('/', function (req, res) {
+
+    let style = '<style> .use-transform { '
+      +'width:100%;'
+      +'height:100%;'
+      +'border:1px solid #000;'
+    +'} '
+    +'.use-transform div{'
+      +'position: relative;'
+      +'width:auto;'
+      +'height:auto;'
+      +'top:50%;'
+      +'transform:translateY(-50%);'
+      //+'background:#095;'
+    +'} h1{ margin-left:50%; }'
+    +'  </style>';
+
+
     let hello = '<h1>' + cool() + '</h1>';
-    res.send(hello);
+    let html = '<div class="use-transform"><div>'+ hello +'</div></div>';
+    //res.send(hello);
+    res.send(html);
     console.log(cool());
 });
-
+//margin:0px auto;
 module.exports = router;
